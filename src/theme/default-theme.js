8cd731859f94a166d6a048b52638043c6cd4af53
Defne Eroglu <defne@appsamurai.com>
2022-03-30 09:50:43 +0300

    install spectacle

---
+++ src/theme/default-theme.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  colors: {
+    primary: 'white',
+    secondary: '#1ac497',
+    tertiary: 'black',
+    quaternary: '#CECECE',
+    backgroundColor: '#151D3B',
+  },
+  space: [0],
+  fonts: {
+    header: '"Helvetica Neue", Helvetica, Arial, sans-serif',
+  },
+  fontSizes: {
+    h1: '72px',
+    h2: '64px',
+  },
+};
